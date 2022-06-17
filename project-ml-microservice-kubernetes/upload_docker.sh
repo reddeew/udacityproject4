9c245f1e4d4b074609edd9ed1f576f952afbc469
EC2 Default User <ec2-user@ip-172-31-92-215.ec2.internal>
2022-06-17 07:11:22 +0000

    final

---
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>                    
+
+
+dockerpath=reddeew/udacity4projectbyeshwar
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login 
+docker image tag nanodegree4 $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker image push $dockerpath
